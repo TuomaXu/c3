7f646a676cb3c6e529c6339e3118d575bd3bd277
tuomaxu <you@example.com>
2019-01-17 10:19:02 +0800

    完成显示所有车辆位置功能

--- main.c
+++ main.c
@@ -105,9 +105,23 @@ int main()
         }
         if (code == 2)
         {
+
         }
         if (code == 3)
         {
+            printf("所有车辆位置如下：\n");
+            
+            for(int i = 0; i < index; i++)
+            {
+                printf("第%d个车辆的位置为(%d,%d)\n",i+1,arr[i].x,arr[i].y);
+            }
+
+            printf("\n点击回车继续\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
+            
+            
         }
         if (code == 4)
         {
