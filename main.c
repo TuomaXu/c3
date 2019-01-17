0d9cfd2314ded368e7cb0116d714c21a67dd9f56
tuomaxu <you@example.com>
2019-01-17 09:50:45 +0800

    完成应用框架

--- main.c
+++ main.c
@@ -21,12 +21,42 @@ arr[1] = 'b';
     5---退出
  */
 
+int main()
+{
 
+    printf("欢迎进入专场位置管理系统!\n");
 
-int main(){
+    while (1)
+    {
+        printf("1---添加车辆位置\n");
+        printf("2---删除车辆位置\n");
+        printf("3---显示全部车辆位置\n");
+        printf("4---返回离我的位置最近的车辆\n");
+        printf("5---退出\n");
 
-    printf("hello world!");
+        printf("请选择相应功能\n");
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+            printf("应用已经退出\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
